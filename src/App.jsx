f2506b7819c57c35aaa627bc01a7340dabd068c3
monica-maker <musictotheworld143@gmail.com>
2024-12-19 16:31:29 +0200

    added a courses cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "/src/components/nav.jsx";
 import Footer from "/src/components/footer.jsx";
 import Header from "/src/components/header.jsx";
+import Courses from "/src/components/cards1.jsx";
+
 export default function App(){
     return(
         <>
             <Nav/>   
-            <Header/> 
+            <Header/>
+            <Courses/> 
             <Footer/>    
         </>
     )
---
+++ src/components/cards1.jsx
@@ -0,0 +1,54 @@
+import "/src/components/styles/cards1.css";
+import { MdOutlineStarPurple500 } from "react-icons/md";
+import { IoIosStarHalf } from "react-icons/io";
+const courses = [
+    {
+        imgSrc: "/src/components/assets/card1.jpg",
+        altText: "",
+        title: "UI/UX Designing Training Course",
+        modules: "11 modules",
+        rating: "4.7"
+    },
+    {
+        imgSrc: "/src/components/assets/card2.jpg",
+        altText: "",
+        title: "Full Stack Development Course",
+        modules: "9 modules",
+        rating: "4.9"
+    },
+    {
+        imgSrc: "/src/components/assets/card3.jpg",
+        altText: "",
+        title: "Digital Marketing & E-Commerce",
+        modules: "15 modules",
+        rating: "4.8"
+    }
+];
+
+export default function Courses() {
+    return (
+        <div className="card-container">
+            {courses.map((course, index) => (
+                <div key={index} className="card">
+                    <div>
+                        <img src={course.imgSrc} alt={course.altText} />
+                        <button>Learn more</button>
+                    </div>
+                    <div className="texts">
+                        <h2>{course.title}</h2>
+                        <h5>{course.modules}</h5>
+                        <div className="icon">
+                            <p>{course.rating}</p>
+                            <MdOutlineStarPurple500 />
+                            <MdOutlineStarPurple500 />
+                            <MdOutlineStarPurple500 />
+                            <MdOutlineStarPurple500 />
+                            <IoIosStarHalf />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
